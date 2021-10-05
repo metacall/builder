439b12a65cf083359435f082003b9947dffeaab9
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2021-10-05 04:33:04 +0200

    Add parsing of arguments.

--- main.go
+++ main.go
@@ -1,36 +1,78 @@
 package main
 
 import (
-	"fmt"
 	"flag"
+	"fmt"
+	"reflect"
 )
 
-type LanguageType string
+type LanguageType int
 
 const (
-	Python LanguageType = "py"
-	Node = "node"
-	TypeScript = "ts"
-	Ruby = "rb"
-	CSharp = "cs"
-	Java = "java"
-	WebAssembly = "wasm"
-	Cobol = "cob"
-	File = "file"
-	Mock = "mock"
-	RPC = "rpc"
+	Python LanguageType = iota
+	Node
+	TypeScript
+	Ruby
+	CSharp
+	Java
+	WebAssembly
+	Cobol
+	File
+	Mock
+	RPC
 )
 
+var LanguageMap = map[string]LanguageType{
+	"py":   Python,
+	"node": Node,
+	"ts":   TypeScript,
+	"rb":   Ruby,
+	"cs":   CSharp,
+	"java": Java,
+	"wasm": WebAssembly,
+	"cob":  Cobol,
+	"file": File,
+	"mock": Mock,
+	"rpc":  RPC,
+}
+
+var LanguageKeys = reflect.ValueOf(LanguageMap).MapKeys()
+
+func (lang LanguageType) String() string {
+	return LanguageKeys[lang].String()
+}
+
 type BuildOptions struct {
-	version string
+	version   string
 	languages []LanguageType
+}
+
+func ParseLanguages(args []string) ([]LanguageType, error) {
+	languages := make([]LanguageType, len(args))
+
+	for i, lang := range args {
+		langId, found := LanguageMap[lang]
+
+		if found != true {
+			return nil, fmt.Errorf("Invalid language, the key '%s' does not exist", lang)
+		}
+
+		languages[i] = langId
+	}
+
+	return languages, nil
 }
 
 func main() {
 	var opt BuildOptions
+	var err error
 	flag.StringVar(&opt.version, "version", "v0.5.6", "MetaCall version to build with")
 	flag.Parse()
-	opt.languages = flag.Args() // TODO
+	opt.languages, err = ParseLanguages(flag.Args())
+
+	if err != nil {
+		panic(err)
+	}
 
 	// Debug
 	fmt.Println(opt.version)
