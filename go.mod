11cfd2cecc85a49e593e5c96691d5e7841ae2cf7
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2021-10-05 05:10:59 +0200

    Solved some bugs and add buildkit.

--- go.mod
+++ go.mod
@@ -1,3 +1,22 @@
 module github.com/metacall/builder
 
 go 1.17
+
+require github.com/moby/buildkit v0.9.1
+
+require (
+	github.com/containerd/containerd v1.5.3 // indirect
+	github.com/docker/distribution v2.7.1+incompatible // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
+	golang.org/x/sys v0.0.0-20210403161142-5e06dd20ab57 // indirect
+	google.golang.org/genproto v0.0.0-20201110150050-8816d57aaa9a // indirect
+	google.golang.org/grpc v1.38.0 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
-	"reflect"
+	"github.com/moby/buildkit/client/llb"
+	"log"
+	"os"
 )
 
 type LanguageType int
@@ -36,10 +39,16 @@ var LanguageMap = map[string]LanguageType{
 	"rpc":  RPC,
 }
 
-var LanguageKeys = reflect.ValueOf(LanguageMap).MapKeys()
+var LanguageKeys = (func() []string {
+	keys := make([]string, len(LanguageMap))
+	for k, v := range LanguageMap {
+		keys[v] = k
+	}
+	return keys
+})()
 
 func (lang LanguageType) String() string {
-	return LanguageKeys[lang].String()
+	return LanguageKeys[lang]
 }
 
 type BuildOptions struct {
@@ -53,8 +62,8 @@ func ParseLanguages(args []string) ([]LanguageType, error) {
 	for i, lang := range args {
 		langId, found := LanguageMap[lang]
 
-		if found != true {
-			return nil, fmt.Errorf("Invalid language, the key '%s' does not exist", lang)
+		if !found {
+			return nil, fmt.Errorf("invalid language, the key '%s' does not exist", lang)
 		}
 
 		languages[i] = langId
@@ -71,10 +80,21 @@ func main() {
 	opt.languages, err = ParseLanguages(flag.Args())
 
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	// Debug
 	fmt.Println(opt.version)
 	fmt.Println(opt.languages)
+
+	// Random instruction for Buildkit in order to test
+	goAlpine := llb.Image("docker.io/library/golang:1.17-alpine")
+
+	dt, err := goAlpine.Marshal(context.TODO(), llb.LinuxAmd64)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	llb.WriteTo(dt, os.Stdout)
 }
