25577c5dcd22f747f97475c1ac991a21e0701d70
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2024-06-18 22:42:43 +0200

    Change runtime to deps in the test.

--- test/test.sh
+++ test/test.sh
@@ -28,7 +28,7 @@ DOCKER_SERVICE=${1:-rootless}
 # Build the languages all together
 echo "Building runtime mode with all languages."
 # export BUILDER_ARGS="runtime py node rb"
-export BUILDER_ARGS="runtime node"
+export BUILDER_ARGS="deps node"
 ${DOCKER_CMD} up --exit-code-from ${DOCKER_SERVICE} ${DOCKER_SERVICE}
 ${DOCKER_CMD} down
 
