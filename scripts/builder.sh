80081bc49e52840f39e2d44a7bfa30dbd097210d
Vicente Eduardo Ferrer Garcia <7854099+viferga@users.noreply.github.com>
2024-11-21 12:09:07 -0500

    Update builder.sh

--- scripts/builder.sh
+++ scripts/builder.sh
@@ -7,14 +7,20 @@ if [ -z "$EXPORT_REGISTRY" ]; then
 fi
 
 if [ -z "$IMPORT_REGISTRY" ]; then
-    IMPORT_REGISTRY="registry:5000/metacall/builder_startup"
+    IMPORT_REGISTRY="registry:5000/metacall/builder_cache"
 fi
 
 if [ -z "$BUILDER_BINARY" ]; then
     BUILDER_BINARY="${HOME}/builder"
 fi
 
+if [ -z "$1" ]; then
+    IMAGE_NAME="builder_output"
+else
+    IMAGE_NAME="builder_output_$1"
+fi
+
 ${BUILDER_BINARY} $@ | buildctl-daemonless.sh build \
     --export-cache type=registry,ref=${EXPORT_REGISTRY},registry.insecure=true \
     --import-cache type=registry,ref=${IMPORT_REGISTRY},registry.insecure=true \
-    --output type=image,name=registry:5000/metacall/builder_output,push=true,registry.insecure=true
+    --output type=image,name=registry:5000/metacall/${IMAGE_NAME},push=true,registry.insecure=true
